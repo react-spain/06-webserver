53dd79a205a6bae990d0d54efa408f298d711b40
Marlom <falconsoft.3d@gmail.com>
2021-02-16 21:42:51 +0100

    s

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 http.createServer( (req, res) => {
-    res.write('Hola Mundo');
+
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+    res.writeHead(200,{  'Content-Type': 'application/csv'  })
+    
+    
+
+    res.write( 'id, nombre\n' );
+    res.write( '1, Pep\n' );
+    res.write( '2, Ramon\n' );
     res.end();
 })
 .listen( 8080 )
